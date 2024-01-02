eaaa41ac9d055612c030c21b9d3c100e6a8b67af
Lidiane Faria <lidianerdefaria@gmail.com>
2024-01-02 20:13:39 -0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  );
 }
 
 export default App;
