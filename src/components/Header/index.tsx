373232287f6c9f8f3e51c2f9639bbb4e8feea65b
Lidiane Faria <lidianerdefaria@gmail.com>
2024-01-03 11:10:48 -0300

    style: create new transaction modal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog';
 
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export const Header = () => {
   return (
@@ -10,16 +11,7 @@ export const Header = () => {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import * as Dialog from '@radix-ui/react-dialog';
+
+export const Overlay = styled(Dialog.Overlay)`
+  background: rgba(0, 0, 0, 0.75);
+  height: 100vh;
+  inset: 0;
+  position: fixed;
+  width: 100vw;
+`;
+
+export const Content = styled(Dialog.Content)`
+  background: ${(props) => props.theme['gray-800']};
+  border-radius: 6px;
+  left: 50%;
+  min-width: 32rem;
+  padding: 2.5rem 3rem;
+  position: fixed;
+  transform: translate(-50%, -50%);
+  top: 50%;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2rem;
+
+    input {
+      background: ${(props) => props.theme['gray-900']};
+      border: 0;
+      border-radius: 6px;
+      color: ${(props) => props.theme['gray-300']};
+      padding: 1rem;
+
+      &::placeholder {
+        color: ${(props) => props.theme['gray-500']};
+      }
+    }
+
+    button[type='submit'] {
+      background: ${(props) => props.theme['green-500']};
+      border: 0;
+      border-radius: 6px;
+      color: ${(props) => props.theme.white};
+      cursor: pointer;
+      font-weight: bold;
+      height: 50px;
+      margin-top: 1.25rem;
+      padding: 0 1.25rem;
+
+      &:hover {
+        background: ${(props) => props.theme['green-700']};
+        transition: background-color 0.2s;
+      }
+    }
+  }
+`;
+
+export const CloseButton = styled(Dialog.Close)`
+  background: transparent;
+  border: 0;
+  color: ${(props) => props.theme['gray-500']};
+  cursor: pointer;
+  line-height: 0;
+  position: absolute;
+  right: 1.5rem;
+  top: 1.5rem;
+`;
