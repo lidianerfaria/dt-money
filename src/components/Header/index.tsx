4af071e85ab76e12c5e2156e72f4b102f69f1c5a
Lidiane Faria <lidianerdefaria@gmail.com>
2024-01-03 10:55:46 -0300

    create accessible modal with radix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <p>Logotipo</p>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
