407cddc369554026d9480205c1342729fa12a42d
Lidiane Faria <lidianerdefaria@gmail.com>
2024-01-15 22:46:21 -0300

    first http request

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
 import { SearchForm } from '../../components/SearchForm';
 import { Summary } from '../../components/Summary';
@@ -8,35 +9,54 @@ import {
   TransactionsTable,
 } from './styles';
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
+
 export const Transactions = () => {
+  const [transactionsData, setTransactionsData] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const loadTransactions = async () => {
+      const response = await fetch('http://localhost:3333/transactions');
+      const data = await response.json();
+      setTransactionsData(data);
+    };
+
+    loadTransactions();
+  }, []);
+  console.log('transactionsData:', transactionsData);
   return (
-    <>
+    <div>
       <Header />
       <Summary />
-
       <TransactionsContainer>
         <SearchForm />
+
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width='50%'>Hambúrguer</td>
-              <td>
-                <PriceHighlight variant='outcome'>-R$ 59,00</PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactionsData.map((it) => {
+              return (
+                <tr key={it.id}>
+                  <td width='50%'>{it.description}</td>
+                  <td>
+                    <PriceHighlight variant={it.type}>
+                      {it.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{it.category}</td>
+                  <td>{it.createdAt}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
-    </>
+    </div>
   );
-};
+}
